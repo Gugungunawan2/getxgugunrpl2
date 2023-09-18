eb40e310b08e539baddb889af786a74e1e4287b4
Gugungunawan2 <141398676+Gugungunawan2@users.noreply.github.com>
2023-09-18 15:04:14 +0700

    Add files via upload

---
+++ lib/home_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class Homecontroller  extends GetxController{
+   
+   TextEditingController namacontroller=TextEditingController();
+
+
+
+
+
+
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'home_controller.dart';
+import 'second_screen.dart';
+
+class homescreen extends StatelessWidget {
+  const homescreen({super.key});
+
+  @override
+Widget build(BuildContext context) {
+    Homecontroller hc=Get.put(Homecontroller());
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Getx'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+           child: TextField(
+            controller: hc.namacontroller,
+            decoration: InputDecoration(labelText: 'Input nama'),
+            onSubmitted: (value) {
+              Get.defaultDialog(title: 'name',middleText: hc.namacontroller.text,onConfirm: () {
+                Get.to(SecondScreen);
+              },);
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                ),
+                onPressed: () { },
+                child: Text('TextButton'),
+              );
+
+              // Get.defaultDialog(
+              //   title:"nama yang di imput",
+              //   middleText:hc.namacontroller.text,
+              //   onConfirm: (){
+              //     Get.to(SecondScreen(),arguments:hc.namacontroller.text);
+              //   });
+            },
+           ),
+          ),
+        ],
+      ),
+    );
+  }
+}
